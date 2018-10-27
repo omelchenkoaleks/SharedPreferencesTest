74de36aab4081b65753d8f15c625052e0fbfa4f8
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-27 17:43:55 +0300

    набраны ресурсы для теста

--- app/src/main/java/com/omelchenkoaleks/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/sharedpreferencestest/MainActivity.java
@@ -1,9 +1,18 @@
 package com.omelchenkoaleks.sharedpreferencestest;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SP_PERSON_DATA = "person_data";
+    private static final String SP_FIRST_NAME = "person_first_name";
+    private static final String SP_LAST_NAME = "person_last_name";
+    private static final String SP_PERSON_AGE = "person_age_info";
+
+    private static SharedPreferences spPerson;
+    private static SharedPreferences.Editor editorPerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
